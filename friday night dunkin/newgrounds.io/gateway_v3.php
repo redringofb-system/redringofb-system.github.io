2da08546d4edbec49a7589812c6723c222c53db9
redringofb-system <73772739+redringofb-system@users.noreply.github.com>
2021-08-20 17:42:54 -0700

    Add files via upload

---
+++ friday night dunkin/newgrounds.io/gateway_v3.php
@@ -0,0 +1,1 @@
+{"success":true,"app_id":"52383:U4EVm6wt","result":{"component":"ScoreBoard.postScore","data":{"success":false,"error":{"message":"User is not logged in.","code":110},"parameters":{"id":10396,"value":-3}}}}
